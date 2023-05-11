1cfe9ae7c1c7d6ae3f84b213bd948d908f58100a
Hugo Chagas <hugochagasuk@gmail.com>
2023-05-11 19:31:07 +0100

    add homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,14 @@
-import Head from 'next/head'
-import {BsFillMoonStarsFill} from 'react-icons/bs';
+import Head from "next/head";
 
-export default function Home() {
+import NavBar from "@/components/NavBar";
+import SocialMedia from "@/components/SocialMedia";
+import HomeIcons from "@/components/HomeIcons";
+import SubNavBar from "@/components/SubNavBar";
+import ProfileInfo from "@/components/ProfileInfo";
+import Footer from "@/components/Footer";
+import BookingBtn from "@/components/BookingBtn";
+
+const Home = () => {
   return (
     <>
       <Head>
@@ -10,22 +17,23 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className='bg-white px-10'>
-        <section className='min-h-screen'>
-          <nav className='py-10 mb-12 flex justify-between'>
-            <h1 className='text-xl'>Milena Sports Massage Therapist</h1>
-            <ul className='flex items-center'>
-              <li>
-                <BsFillMoonStarsFill className='cursor-pointer text-xl'/>
-              </li>
-              
-              <li>
-                <a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href="#">Resume</a>
-              </li>
-            </ul>
-          </nav>
-        </section>       
+
+      <main className="bg-white px-10">
+        <section className="min-h-screen">
+          <NavBar />
+          <SubNavBar />
+          <ProfileInfo />
+          <BookingBtn />
+          <SocialMedia />
+        </section>
+
+        <section className="min-h-screen">
+          <HomeIcons />
+          <Footer />
+        </section>
       </main>
     </>
-  )
-}
+  );
+};
+
+export default Home;
