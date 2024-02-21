81711f4d48a83f90363b3f9189332d5f473ae2a2
Hugo Chagas <hugochagasuk@gmail.com>
2024-02-21 20:56:32 +0000

    update prices - split into mobile and treatment room prices

--- src/pages/pricing.js
+++ src/pages/pricing.js
@@ -26,26 +26,18 @@ const Pricing = () => {
             <div className="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white">
               <h3 className="mb-4 text-2xl font-semibold">60 min</h3>
               {/* <p className="font-light text-gray-500 sm:text-lg dark:text-gray-400">Best option for personal use & for your next project.</p> */}
-              <div className="flex justify-center items-baseline my-8">
-                <span className="mr-2 text-5xl font-extrabold">£ 50</span>
+              <div className="flex flex-col justify-center items-center my-8">
+                <div className="flex flex-col justify-center items-center my-2">
+                  <span className="mr-2 text-5xl font-extrabold pb-4">£ 45</span>
+                  <span className="text-lg -mt-3">Treatment room</span>
+                </div>
+                  <div className="flex flex-col justify-center items-center my-2">
+                    <span className="mr-2 text-2xl font-extrabold">£ 55</span>
+                    <span className="text-md -mt-1">Mobile treatment</span>
+                  </div>
                 {/* <span className="text-gray-500 dark:text-gray-400">/month</span> */}
               </div>
               <ul role="list" className="mb-8 space-y-4 text-left">
-                <li className="flex items-center space-x-3">
-                  <svg
-                    className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                      clip-rule="evenodd"
-                    ></path>
-                  </svg>
-                  <span>Mobile treatment</span>
-                </li>
                 <li className="flex items-center space-x-3">
                   <svg
                     className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
@@ -104,26 +96,18 @@ const Pricing = () => {
             <div className="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white">
               <h3 className="mb-4 text-2xl font-semibold">90 min</h3>
               {/* <p className="font-light text-gray-500 sm:text-lg dark:text-gray-400">Best option for personal use & for your next project.</p> */}
-              <div className="flex justify-center items-baseline my-8">
-                <span className="mr-2 text-5xl font-extrabold">£ 70</span>
+              <div className="flex flex-col justify-center items-center my-8">
+                <div className="flex flex-col justify-center items-center my-2">
+                  <span className="mr-2 text-5xl font-extrabold pb-4">£ 65</span>
+                  <span className="text-lg -mt-3">Treatment room</span>
+                </div>
+                  <div className="flex flex-col justify-center items-center my-2">
+                    <span className="mr-2 text-2xl font-extrabold">£ 75</span>
+                    <span className="text-md -mt-1">Mobile treatment</span>
+                  </div>
                 {/* <span className="text-gray-500 dark:text-gray-400">/month</span> */}
               </div>
               <ul role="list" className="mb-8 space-y-4 text-left">
-                <li className="flex items-center space-x-3">
-                  <svg
-                    className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                      clip-rule="evenodd"
-                    ></path>
-                  </svg>
-                  <span>Mobile treatment</span>
-                </li>
                 <li className="flex items-center space-x-3">
                   <svg
                     className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
@@ -182,26 +166,18 @@ const Pricing = () => {
             <div className="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white">
               <h3 className="mb-4 text-2xl font-semibold">120 min</h3>
               {/* <p className="font-light text-gray-500 sm:text-lg dark:text-gray-400">Best option for personal use & for your next project.</p> */}
-              <div className="flex justify-center items-baseline my-8">
-                <span className="mr-2 text-5xl font-extrabold">£ 90</span>
+              <div className="flex flex-col justify-center items-center my-8">
+                <div className="flex flex-col justify-center items-center my-2">
+                  <span className="mr-2 text-5xl font-extrabold pb-4">£ 85</span>
+                  <span className="text-lg -mt-3">Treatment room</span>
+                </div>
+                  <div className="flex flex-col justify-center items-center my-2">
+                    <span className="mr-2 text-2xl font-extrabold">£ 105</span>
+                    <span className="text-md -mt-1">Mobile treatment</span>
+                  </div>
                 {/* <span className="text-gray-500 dark:text-gray-400">/month</span> */}
               </div>
               <ul role="list" className="mb-8 space-y-4 text-left">
-                <li className="flex items-center space-x-3">
-                  <svg
-                    className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                      clip-rule="evenodd"
-                    ></path>
-                  </svg>
-                  <span>Mobile treatment</span>
-                </li>
                 <li className="flex items-center space-x-3">
                   <svg
                     className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
@@ -259,27 +235,15 @@ const Pricing = () => {
             {/* Card 4 */}
             <div className="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white">
               <h3 className="mb-4 text-xl font-semibold">Beauty Facial Express</h3>
-              <p className="font-light text-gray-500 sm:text-lg dark:text-gray-400">30 min</p>
-              <div className="flex justify-center items-baseline my-8">
-                <span className="mr-2 text-5xl font-extrabold">£ 30</span>
+              <div className="flex flex-col justify-center items-center my-8">
+                <div className="flex flex-col justify-center items-center my-2">
+                  <span className="mr-2 text-5xl font-extrabold pb-4">£ 35</span>
+                  {/* <span className="text-lg -mt-3">Treatment room</span> */}
+                </div>
                 {/* <span className="text-gray-500 dark:text-gray-400">/month</span> */}
+                <p className="font-light text-gray-500 sm:text-lg dark:text-gray-400">30 min</p>
               </div>
-              <ul role="list" className="mb-8 space-y-4 text-left">
-                <li className="flex items-center space-x-3">
-                  <svg
-                    className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                      clip-rule="evenodd"
-                    ></path>
-                  </svg>
-                  <span>Mobile treatment</span>
-                </li>
+              <ul role="list" className="mb-8 mt-20 space-y-4 text-left">
                 <li className="flex items-center space-x-3">
                   <svg
                     className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
@@ -308,7 +272,7 @@ const Pricing = () => {
                       clip-rule="evenodd"
                     ></path>
                   </svg>
-                  <span>Facial pampering with massage</span>
+                  <span>Facial treatment with facial uplifting massage</span>
                 </li>
                 <li className="flex items-center space-x-3">
                   <svg
