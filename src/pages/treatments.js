77de0d4e62425e18c0e4fb58b771d16969b3d2aa
Hugo Chagas <hugochagasuk@gmail.com>
2024-02-22 02:22:10 +0000

    treatments.js page update Swedish Relaxing Massage to become a Link, add alt properties to all Link tags (cards), and add Cupping Therapy, Pregnancy Massage and Electrotherapy cards

--- src/pages/treatments.js
+++ src/pages/treatments.js
@@ -16,14 +16,14 @@ const Treatments = () => {
 
         {/* Card 1 */}
         
-          <div
+          <Link
           href="/contact"
           className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 my-10"
         >
           <img
             className="object-cover w-full rounded-t-lg h-72 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
             src="\assets\images\deeptissue_img.jpg"
-            alt=""
+            alt="Swedish Relaxing Massage"
           />
           <div className="flex flex-col justify-between p-4 leading-normal">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
@@ -50,10 +50,8 @@ const Treatments = () => {
             </div>
 
           </div>
-        </div>
+        </Link>
         
-        
-
 
         {/* Card 2 */}
         <Link
@@ -63,7 +61,7 @@ const Treatments = () => {
           <img
             className="object-cover w-full rounded-t-lg h-72 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
             src="\assets\images\swedish_img.jpeg"
-            alt=""
+            alt="Deep Tissue Massage"
           />
           <div className="flex flex-col justify-between p-4 leading-normal">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
@@ -90,6 +88,8 @@ const Treatments = () => {
           </div>
         </Link>
 
+
+
         {/* Card 3 */}
         <Link
           href="/contact"
@@ -98,7 +98,7 @@ const Treatments = () => {
           <img
             className="object-cover w-full rounded-t-lg h-72 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
             src="\assets\images\sports_img.jpg"
-            alt=""
+            alt="Sports massage"
           />
           <div className="flex flex-col justify-between p-4 leading-normal">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
@@ -126,6 +126,115 @@ const Treatments = () => {
         </Link>
 
         {/* Card 4 */}
+        <Link
+          href="/contact"
+          className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 my-10"
+        >
+          <img
+            className="object-cover w-full rounded-t-lg h-72 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+            src="\assets\images\cupping.jpg"
+            alt="Cupping Therapy"
+          />
+          <div className="flex flex-col justify-between p-4 leading-normal">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Cupping Therapy
+            </h5>
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            Cups are placed on the skin to create a gentle suction. This helps to increase blood flow, break out adhesions, release toxins, and stimulate the body's natural healing process.
+            </p>
+            <div className="flex justify-center gap-8 mt-5">
+              <Link
+                  href="/pricing"
+                  className="text-white bg-green-300 hover:bg-green-500 focus:ring-4 focus:ring-green-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-green-900"
+                >
+                  Check price
+                </Link>
+
+                <Link
+                  href="/contact"
+                  className="text-white bg-green-300 hover:bg-green-500 focus:ring-4 focus:ring-green-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-green-900"
+                >
+                  Book now
+                </Link>
+            </div>
+          </div>
+        </Link>
+
+
+        {/* Card 5 */}
+        <Link
+          href="/contact"
+          className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 my-10"
+        >
+          <img
+            className="object-cover w-full rounded-t-lg h-72 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+            src="\assets\images\pregnancy_massage.jpg"
+            alt="Pregnancy Massage"
+          />
+          <div className="flex flex-col justify-between p-4 leading-normal">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Pregnancy Massage
+            </h5>
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            Pregnancy massage focus on addressing pregnancy-related discomforts and promoting relaxation.
+            </p>
+            <div className="flex justify-center gap-8 mt-5">
+              <Link
+                  href="/pricing"
+                  className="text-white bg-green-300 hover:bg-green-500 focus:ring-4 focus:ring-green-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-green-900"
+                >
+                  Check price
+                </Link>
+
+                <Link
+                  href="/contact"
+                  className="text-white bg-green-300 hover:bg-green-500 focus:ring-4 focus:ring-green-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-green-900"
+                >
+                  Book now
+                </Link>
+            </div>
+          </div>
+        </Link>
+
+
+         {/* Card 6 */}
+        <Link
+          href="/contact"
+          className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 my-10"
+        >
+          <img
+            className="object-cover w-full rounded-t-lg h-72 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+            src="\assets\images\electrotherapy.png"
+            alt="Electrotherapy"
+          />
+          <div className="flex flex-col justify-between p-4 leading-normal">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            Electrotherapy
+            </h5>
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+            Electrotherapy involves the use of electrical devices or equipment to complement traditional massage techniques, the electrical currents stimulate tissue repair and reduce pain.
+            Electrotherapy can be used in combination with massage to enhance the therapeutic effects and provide relief for various musculoskeletal conditions.
+            </p>
+            <div className="flex justify-center gap-8 mt-5">
+              <Link
+                  href="/pricing"
+                  className="text-white bg-green-300 hover:bg-green-500 focus:ring-4 focus:ring-green-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-green-900"
+                >
+                  Check price
+                </Link>
+
+                <Link
+                  href="/contact"
+                  className="text-white bg-green-300 hover:bg-green-500 focus:ring-4 focus:ring-green-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-green-900"
+                >
+                  Book now
+                </Link>
+            </div>
+          </div>
+        </Link>
+
+
+        {/* Card 7 */}
         <Link
           href="/contact"
           className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 my-10"
@@ -160,7 +269,7 @@ const Treatments = () => {
           </div>
         </Link>
 
-        {/* Card 5 */}
+        {/* Card 8 */}
         <Link
           href="/contact"
           className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 my-10"
