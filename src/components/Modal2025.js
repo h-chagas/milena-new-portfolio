24a881e3c7ff664b98f9085c88fa457c489f65f6
Hugo Chagas <hugochagasuk@gmail.com>
2025-01-04 18:04:10 +0000

    modal

--- src/components/Modal2025.js
+++ src/components/Modal2025.js
@@ -7,8 +7,8 @@ const Modal = ({ showModal, setShowModal }) => {
   return (
     <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center">
       <div className="bg-white p-5 rounded shadow-lg">
-        <h2 className="text-xl font-bold mb-4">Important Information</h2>
-        <p>This is an important message for the users.</p>
+        <h2 className="text-xl font-bold mb-4"> Reduced Availability Notice </h2>
+        <p>Please be advised that my availability is currently limited. If you send me a message, please understand that there may be a delay in my response.</p>
         <button 
           onClick={() => setShowModal(false)} 
           className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
